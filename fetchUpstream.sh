3bd1f9e9b79c39e88e3ede5f3fe76cf6ecd4f099
tr7zw <tr7zw@live.de>
2020-07-09 18:12:28 +0200

    Update Upstream

--- fetchUpstream.sh
+++ fetchUpstream.sh
@@ -1,6 +1,9 @@
 cd Tuinity
+git clean -fx
+git clean -fd
 git fetch
-git reset --hard origin/head
+git reset --hard origin/master
+git submodule update --init --recursive -f
 cd ..
 sh patchPaper.sh
 cd Tuinity
