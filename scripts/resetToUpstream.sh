88a00b8736c71e0e638d779fa212dac85bbb7f98
Bud Gidiere <sgidiere@gmail.com>
2020-07-17 20:11:45 -0500

    fixes

--- scripts/resetToUpstream.sh
+++ scripts/resetToUpstream.sh
@@ -5,33 +5,37 @@ basedir=$1
 basename=$(basename $baseproject)
 branch=HEAD
 for type in ${searchtxts[@]}; do
-	baseproject=/Tuinity/Tuinity-$type
+	baseproject=Tuinity/Tuinity-$type
 	target=YAPFA-$type
+	echo "$baseproject, $target, $branch, $basedir, $type, $basename"
 	echo "Setup upstream project.."
+	echo "$basedir/$baseproject"
 	cd "$basedir/$baseproject"
-	$gitcmd fetch --all &> /dev/null
+	git fetch --all &> /dev/null
 	# Create the upstream branch in Paper project with current state
-	$gitcmd checkout master >/dev/null 2>&1 # possibly already in
-	$gitcmd branch -D upstream &> /dev/null
-	$gitcmd branch -f upstream "$branch" &> /dev/null && $gitcmd checkout upstream &> /dev/null
+	git checkout master >/dev/null 2>&1 # possibly already in
+	git branch -D upstream &> /dev/null
+	git branch -f upstream "$branch" &> /dev/null && git checkout upstream &> /dev/null
     cd $basedir
 	# Create source project dirs
     if [ ! -d  "$basedir/$target" ]; then
         mkdir "$basedir/$target"
         cd "$basedir/$target"
-        # $gitcmd remote add origin "$5"
+        # git remote add origin "$5"
 	fi
+	echo "$basedir/$target"
     cd "$basedir/$target"
-	$gitcmd init > /dev/null 2>&1
+	git init > /dev/null 2>&1
 
     echo "  "
 	echo "Reset $target to $basename.."
 	# Add the generated Paper project as the upstream remote of subproject
-    $gitcmd remote rm upstream &> /dev/null
-    $gitcmd remote add upstream "$basedir/$baseproject" &> /dev/null
+    git remote rm upstream &> /dev/null
+    git remote add upstream "$basedir/$baseproject" &> /dev/null
 	# Ensure that we are in the branch we want so not overriding things
-    $gitcmd checkout master &> /dev/null || $gitcmd checkout -b master &> /dev/null
-    $gitcmd fetch upstream &> /dev/null
+    git checkout master &> /dev/null || git checkout -b master &> /dev/null
+    git fetch upstream &> /dev/null
 	# Reset our source project to Paper
-    cd "$basedir/$target" && $gitcmd reset --hard upstream/upstream &> /dev/null
+    cd "$basedir/$target" && git reset --hard upstream/upstream &> /dev/null
 done
+
