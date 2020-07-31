c2b2481776b42a247612cb70eb297dc13128a1a9
Bud Gidiere <sgidiere@gmail.com>
2020-07-31 18:14:46 -0500

    time to do some fixin' :eyes:

--- scripts/applyPatches.sh
+++ scripts/applyPatches.sh
@@ -37,7 +37,7 @@ function applyPatch {
 	    echo "  $(bashcolor 1 33)($5/$6) Skipped$(bashcolorend) - No patch found for $target under patches/$patch_folder"
 		return
 	fi
-	git checkout -b $2
+	
 	# Disable GPG signing before AM, slows things down and doesn't play nicely.
 	# There is also zero rational or logical reason to do so for these sub-repo AMs.
 	# Calm down kids, it's re-enabled (if needed) immediately after, pass or fail.
@@ -63,6 +63,7 @@ function applyPatch {
 	# Apply our patches on top Paper in our dirs
     #$gitcmd am --no-utf8 --3way --ignore-whitespace "$basedir/patches/$patch_folder/"*.patch
 	cd $basedir/$2
+	git checkout -b $2-upstream
 	for filename in $basedir/patches/$patch_folder/*.patch; do
 		# Abort previous applying operation
 		git am --abort >/dev/null 2>&1
@@ -101,6 +102,7 @@ function applyPatch {
 }
 $1/scripts/resetToUpstream.sh $1
 $1/scripts/getUpstream.sh $1
+
 (applyPatch YAPFA/YAPFA-API ${FORK_NAME}-API HEAD api $API_REPO 0 2 &&
 applyPatch YAPFA/YAPFA-Server ${FORK_NAME}-Server HEAD server $SERVER_REPO 1 2 && enableCommitSigningIfNeeded) || (
 enableCommitSigningIfNeeded
