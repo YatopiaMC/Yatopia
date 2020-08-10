8d2dbe2148cec19809d4c162cdaa0f489d1f50de
Bud Gidiere <sgidiere@gmail.com>
2020-08-10 14:57:20 -0500

    fix script

--- scripts/importSources.sh
+++ scripts/importSources.sh
@@ -143,7 +143,7 @@ done
 # NMS import format:
 # importToPaperWorkspace MinecraftServer
 if [ $forkname = "Yatopia" ]; then
-    
+    echo "Adding Dev Extra Imports"
 fi
 # Library import format (multiple files are supported):
 # importLibraryToPaperWorkspace com.mojang datafixerupper com/mojang/datafixers/util Either.java
