c64682be03edd9a3bca9b79b49ffbeb4123e6374
ishland <ishlandmc@yeah.net>
2020-10-25 13:45:41 +0800

    You forgot to commit a file again

--- Yatopia-Hwaccel/src/main/java/net/yatopia/hwaccel/lighting/structures/Chunk.java
+++ Yatopia-Hwaccel/src/main/java/net/yatopia/hwaccel/lighting/structures/Chunk.java
@@ -1,10 +1,32 @@
 package net.yatopia.hwaccel.lighting.structures;
 
+import java.util.Objects;
+
 public abstract class Chunk { // TODO add api
 
-    final ChunkPosition chunkPosition;
+    public final ChunkPosition chunkPosition;
 
     public Chunk(ChunkPosition chunkPosition) {
         this.chunkPosition = chunkPosition;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Chunk chunk = (Chunk) o;
+        return chunkPosition.equals(chunk.chunkPosition);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(chunkPosition);
+    }
+
+    @Override
+    public String toString() {
+        return "Chunk{" +
+                "chunkPosition=" + chunkPosition +
+                '}';
     }
 }
