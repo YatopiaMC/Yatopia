774c975e473407f63462518c2f50dbe12042b546
Zoe <duplexsys@protonmail.com>
2021-01-18 13:02:30 -0600

    Sidestream Support For Commit Message

--- buildSrc/src/main/kotlin/task/UpstreamCommit.kt
+++ buildSrc/src/main/kotlin/task/UpstreamCommit.kt
@@ -7,6 +7,9 @@ import org.gradle.api.Task
 import taskGroup
 import toothpick
 import upstreamDir
+import upstreams
+import java.io.File
+import Upstream
 
 internal fun Project.createUpstreamCommitTask(
     receiver: Task.() -> Unit = {}
@@ -14,20 +17,69 @@ internal fun Project.createUpstreamCommitTask(
     receiver(this)
     group = taskGroup
     doLast {
-        val oldRev = ensureSuccess(gitCmd("ls-tree", "HEAD", toothpick.upstream))
-            ?.substringAfter("commit ")?.substringBefore("\t")
-        val gitChangelog =
-            ensureSuccess(gitCmd("log", "--oneline", "$oldRev...HEAD", printOut = true, dir = upstreamDir)) {
-                logger.lifecycle("No upstream changes to commit?")
+        val changedUpstreams = ArrayList<String>()
+        var gitChangelog = ""
+
+        gitChangelog += getUpstreamChanges(project,this, changedUpstreams, gitChangelog, toothpick.upstream,
+            upstreamDir, toothpick.upstream)
+
+        for (upstream in upstreams) {
+            gitChangelog += getUpstreamChanges(project,this, changedUpstreams, gitChangelog, upstream.name,
+                upstream.repoPath.toFile(), "upstream/${upstream.name}")
+        }
+
+        val changedUpstreamsString = ""
+        for (upstreamName in changedUpstreams) {
+            if (changedUpstreams.isNotEmpty()) {
+                changedUpstreams += "/"
             }
-        val commitMessage = """
-                    |Updated Upstream (${toothpick.upstream})
+            changedUpstreams += upstreamName
+        }
+
+        if (changedUpstreamsString.isNotEmpty()) {
+            val commitMessage = """
+                    |Updated Upstream and Sidestream(s) ($changedUpstreamsString)
                     |
-                    |Upstream has released updates that appear to apply and compile correctly
+                    |Upstream/An Sidestream has released updates that appears to apply and compile correctly
+                    |This update has NOT been tested by YatopiaMC and as with ANY update, please do your own testing.
                     |
-                    |${toothpick.upstream} Changes:
+                    |
                     |$gitChangelog
                 """.trimMargin()
-        ensureSuccess(gitCmd("commit", "-m", commitMessage, printOut = true))
+            ensureSuccess(gitCmd("commit", "-m", commitMessage, printOut = true))
+        }
+
     }
+}
+
+private fun getUpstreamChanges(
+    project: Project,
+    task: Task,
+    changedUpstreams: ArrayList<String>,
+    gitChangelog: String,
+    name: String,
+    dir: File,
+    path: String
+): String {
+    var gitChangelog1 = gitChangelog
+    var oldRev = ensureSuccess(project.gitCmd("ls-tree", "HEAD", path))
+        ?.substringAfter("commit ")?.substringBefore("\t")
+    var changes = true;
+    var upstreamTmp = ensureSuccess(
+        project.gitCmd(
+            "log",
+            "--oneline",
+            "$oldRev...HEAD",
+            printOut = true,
+            dir = dir
+        )
+    ) {
+        changes = false;
+        task.logger.lifecycle("No $name changes to commit.")
+    }
+    if (changes) {
+        changedUpstreams.add("$name")
+        gitChangelog1 += "$name Changes:\n$upstreamTmp\n"
+    }
+    return gitChangelog1
 }
