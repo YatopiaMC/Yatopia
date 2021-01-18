64c8eae69b041bf2e7526583c4098a4d7df3ff19
Zoe <duplexsys@protonmail.com>
2021-01-17 18:21:14 -0600

    Don't clone unnecessary submodules

--- buildSrc/src/main/kotlin/task/InitGitSubmodules.kt
+++ buildSrc/src/main/kotlin/task/InitGitSubmodules.kt
@@ -5,15 +5,19 @@ import org.gradle.api.Project
 import org.gradle.api.Task
 import taskGroup
 import upstreamDir
+import upstreams
 
 internal fun Project.createInitGitSubmodulesTask(
     receiver: Task.() -> Unit = {}
 ): Task = tasks.create("initGitSubmodules") {
     receiver(this)
     group = taskGroup
-    onlyIf { !upstreamDir.resolve(".git").exists() }
+    var upstreamNotInit = false
+    for (upstream in upstreams) { upstreamNotInit = upstreamNotInit || upstream.repoPath.toFile().resolve(".git").exists() }
+    onlyIf { !upstreamDir.resolve(".git").exists() || upstreamNotInit }
     doLast {
-        val exit = gitCmd("submodule", "update", "--init", "--recursive", printOut = true).exitCode
+        var exit = gitCmd("submodule", "update", "--init", printOut = true).exitCode
+        exit += gitCmd("submodule", "update", "--init", "--recursive", dir = upstreamDir,printOut = true).exitCode
         if (exit != 0) {
             error("Failed to checkout git submodules: git exited with code $exit")
         }
