df8005bb466848c62ddbf01265e50595bd22838a
Zoe <duplexsys@protonmail.com>
2021-01-19 13:18:14 -0600

    Revert "try not updating sub-modules "
    This reverts commit 1c3d1b905f58668856c53e62b20dc7de35dd3c14.

--- buildSrc/src/main/kotlin/task/InitGitSubmodules.kt
+++ buildSrc/src/main/kotlin/task/InitGitSubmodules.kt
@@ -16,8 +16,8 @@ internal fun Project.createInitGitSubmodulesTask(
     for (upstream in upstreams) { upstreamNotInit = upstreamNotInit || upstream.repoPath.toFile().resolve(".git").exists() }
     onlyIf { !upstreamDir.resolve(".git").exists() || upstreamNotInit }
     doLast {
-        var exit = gitCmd("submodule", "init", printOut = true).exitCode
-        exit += gitCmd("submodule", "init", dir = upstreamDir, printOut = true).exitCode
+        var exit = gitCmd("submodule", "update", "--init", printOut = true).exitCode
+        exit += gitCmd("submodule", "update", "--init", "--recursive", dir = upstreamDir, printOut = true).exitCode
         if (exit != 0) {
             error("Failed to checkout git submodules: git exited with code $exit")
         }
