28c2af27cdfcf9c6bdeaac02529a58a70bc4d348
Zoe <duplexsys@protonmail.com>
2021-01-19 12:22:43 -0600

    Fix Branch Issue

--- buildSrc/src/main/kotlin/task/RebuildPatches.kt
+++ buildSrc/src/main/kotlin/task/RebuildPatches.kt
@@ -37,7 +37,9 @@ internal fun Project.createRebuildPatchesTask(
                 patchPath.listFiles()
                     ?.filter { it -> it.name.endsWith(".patch") }
                     ?.forEach { it -> it.delete() }
-                ensureSuccess(gitCmd("checkout", "${upstream.name}-$folder", dir = projectDir))
+                System.out.println(projectDir)
+                ensureSuccess(gitCmd("checkout", "-B", "${upstream.name}-$folder", dir = projectDir,
+                    printOut = true))
                 ensureSuccess(
                     gitCmd(
                         "format-patch",
@@ -49,7 +51,7 @@ internal fun Project.createRebuildPatchesTask(
                 )
                 previousUpstreamName = "${upstream.name}-$folder"
             }
-            ensureSuccess(gitCmd("checkout", "$forkName-$folder", dir = projectDir))
+            ensureSuccess(gitCmd("checkout", "-B", "$forkName-$folder", dir = projectDir))
             if (!patchesDir.exists()) {
                 patchesDir.mkdirs()
             }
