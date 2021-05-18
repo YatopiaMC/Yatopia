bcb2db414db3b5c69a1c287c4bc77d36f1145892
tofikarz <62406295+tofikarz@users.noreply.github.com>
2021-05-18 17:17:28 +0200

    done , ready to merge

--- buildSrc/src/main/kotlin/task/Paperclip.kt
+++ buildSrc/src/main/kotlin/task/Paperclip.kt
@@ -12,7 +12,7 @@ import toothpick
 internal fun Project.createPaperclipTask(
     receiver: Task.() -> Unit = {}
 ): Task = tasks.create("paperclip") {
-    dependsOn("yatopia-server:jar")
+    dependsOn(":yatopia-server:jar")
     receiver(this)
     group = taskGroup
     doLast {
