34106bb1fecfabc89b03f8d04992464831025ff2
Tofik <slodkitofik@gmail.com>
2021-06-13 23:05:30 +0200

    Switch to paperweight

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,21 +1,10 @@
-import java.util.Locale
-
-val forkName = "Yatopia"
-val forkNameLowercase = forkName.toLowerCase(Locale.ENGLISH)
-
-rootProject.name = forkNameLowercase
-
-setupSubproject("$forkNameLowercase-api") {
-    projectDir = File("$forkName-API")
-    buildFileName = "../subprojects/api.gradle.kts"
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven("https://wav.jfrog.io/artifactory/repo/")
+    }
 }
-setupSubproject("$forkNameLowercase-server") {
-    projectDir = File("$forkName-Server")
-    buildFileName = "../subprojects/server.gradle.kts"
-}
-setupSubproject("Yatoclip") { }
 
-inline fun setupSubproject(name: String, block: ProjectDescriptor.() -> Unit) {
-    include(name)
-    project(":$name").apply(block)
-}
+rootProject.name = "Yatopia"
+
+include("Yatopia-API", "Yatopia-Server")
